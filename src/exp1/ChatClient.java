f326024427b38c3285ecd4673753af5dc8fd695f
shiva1718 <ms.shiva2019@gmail.com>
2023-06-12 23:04:34 +0530

    Created EchoServer and EchoClient classes for experiment no.2

--- src/exp1/ChatClient.java
+++ src/exp1/ChatClient.java
@@ -1,4 +1,34 @@
 package exp1;
 
+import ports.Ports;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            Socket socket = new Socket("localhost", Ports.CHAT.getPort());
+            OutputStream outputStream = socket.getOutputStream();
+            System.out.println("Enter your message: ");
+            String message = sc.nextLine();
+            outputStream.write(message.getBytes());
+            outputStream.close();
+            socket = new Socket("localhost", Ports.CHAT.getPort());
+            InputStream inputStream = socket.getInputStream();
+            System.out.println("Reply:");
+            String reply = new String(inputStream.readAllBytes());
+            System.out.println(reply);
+            if (message.equalsIgnoreCase("bye") || message.equalsIgnoreCase("quit")) {
+                socket.close();
+                break;
+            }
+            inputStream.close();
+        }
+    }
 }
--- src/exp1/ChatServer.java
+++ src/exp1/ChatServer.java
@@ -1,4 +1,38 @@
 package exp1;
 
+import ports.Ports;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(Ports.CHAT.getPort());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            Socket accept = serverSocket.accept();
+            InputStream inputStream = accept.getInputStream();
+//            System.out.println("Client connected: " + accept.getInetAddress());
+            String message = new String(inputStream.readAllBytes());
+            System.out.println("Client message:");
+            System.out.println(message);
+            accept.close();
+            accept = serverSocket.accept();
+            OutputStream outputStream = accept.getOutputStream();
+            System.out.println("Enter your message: ");
+            String reply = sc.nextLine();
+            outputStream.write(reply.getBytes());
+            System.out.println();
+            if (message.equalsIgnoreCase("bye") || message.equalsIgnoreCase("quit")) {
+                serverSocket.close();
+                break;
+            }
+            accept.close();
+        }
+
+    }
 }
