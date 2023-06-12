35a58feaae4eb69ec6c5888770a348067eb65ca1
shiva1718 <ms.shiva2019@gmail.com>
2023-06-12 23:19:12 +0530

    Created EchoServer and EchoClient classes for experiment no.2

--- src/exp1/FileTransferClient.java
+++ src/exp1/FileTransferClient.java
@@ -1,4 +1,14 @@
 package exp1;
 
+import ports.Ports;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class FileTransferClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", Ports.FILE_TRANSFER.getPort());
+        socket.getInputStream().transferTo(System.out);
+        socket.close();
+    }
 }
--- src/exp1/FileTransferServer.java
+++ src/exp1/FileTransferServer.java
@@ -1,4 +1,19 @@
 package exp1;
 
+import ports.Ports;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FileTransferServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(Ports.FILE_TRANSFER.getPort());
+        Socket accept = serverSocket.accept();
+        new FileInputStream("sample.txt").transferTo(accept.getOutputStream());
+        accept.close();
+        serverSocket.close();
+        System.out.println("File transferred successfully!");
+    }
 }
